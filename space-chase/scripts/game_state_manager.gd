234442820303cdd1804dde3e2129c3ad65279f6c
khromeengine <jianhengzhou003@gmail.com>
2024-11-29 15:18:06 -0800

    Update game_state_manager.gd

--- space-chase/scripts/game_state_manager.gd
+++ space-chase/scripts/game_state_manager.gd
@@ -1,5 +1,6 @@
 extends Node # No class name because autoloaded script
 
+
 """
 Stores and manages energy between players
 - Held energy (with methods to add/remove energy)
@@ -18,3 +19,34 @@ enum PlayerID {
 	PLAYER_1,
 	PLAYER_2,
 }
+
+var _active_camera: Camera2D
+var _player_points: Vector2i = Vector2i.ZERO
+var _total_distance: float = 0.0
+var _camera_pos: float = 0.0
+var _level_progress: float = 0.0
+
+
+func set_active_camera(new_camera: Camera2D) -> void:
+	_active_camera.enabled = false
+	new_camera.enabled = true
+	_active_camera = new_camera
+
+
+func set_player_energy(energy: int, player: PlayerID) -> void:
+	_player_points[player] = energy
+
+
+func add_player_energy(energy: int, player: PlayerID) -> void:
+	_player_points[player] += energy
+
+
+# Used instead of a camera reference.
+# This is called from the camera instead of using camera ref
+# in process to update.
+func update_camera_pos(new_camera_pos: float) -> void:
+	_camera_pos = new_camera_pos
+
+
+func calculate_victory() -> PlayerID:
+	return PlayerID.PLAYER_1 if _player_points[0] > _player_points[1] else PlayerID.PLAYER_2
