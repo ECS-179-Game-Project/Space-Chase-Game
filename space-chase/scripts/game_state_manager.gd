0a6926b6852638ddcd0d56847781e471962b28fe
khromeengine <jianhengzhou003@gmail.com>
2024-11-29 15:34:47 -0800

    Update game_state_manager.gd
    Added documentation

--- space-chase/scripts/game_state_manager.gd
+++ space-chase/scripts/game_state_manager.gd
@@ -27,26 +27,39 @@ var _camera_pos: float = 0.0
 var _level_progress: float = 0.0
 
 
+## Sets the active camera to [param new_camera].
+## This disables the current camera and enables [param new_camera].
+## Assumes _active_camera is set on world ready.
+## @experimental: Incomplete or unnecessary. 
 func set_active_camera(new_camera: Camera2D) -> void:
-	_active_camera.enabled = false
+	if _active_camera != null:
+		_active_camera.enabled = false
 	new_camera.enabled = true
 	_active_camera = new_camera
 
 
-func set_player_energy(energy: int, player: PlayerID) -> void:
-	_player_points[player] = energy
+## Sets the score of player with ID [param id] to [param energy]
+func set_player_energy(energy: int, id: PlayerID) -> void:
+	_player_points[id] = energy
 
 
-func add_player_energy(energy: int, player: PlayerID) -> void:
-	_player_points[player] += energy
+## Increases the score of player with ID [param id] by [param energy]
+func add_player_energy(energy: int, id: PlayerID) -> void:
+	_player_points[id] += energy
 
 
-# Used instead of a camera reference.
-# This is called from the camera instead of using camera ref
-# in process to update.
-func update_camera_pos(new_camera_pos: float) -> void:
+## Used instead of a camera reference.
+## This is called from the camera instead of using camera ref
+## in process to update.
+func set_camera_pos(new_camera_pos: float) -> void:
 	_camera_pos = new_camera_pos
 
 
-func calculate_victory() -> PlayerID:
+## Returns the current level progress of the camera.
+func get_level_progress() -> float:
+	return _total_distance / _level_progress
+
+
+## Returns the winning player based on current score.
+func get_player_victory() -> PlayerID:
 	return PlayerID.PLAYER_1 if _player_points[0] > _player_points[1] else PlayerID.PLAYER_2
