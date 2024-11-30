e7e0bb31688a6901b3326ba9e55f9934a05fae3f
khromeengine <jianhengzhou003@gmail.com>
2024-11-30 01:19:47 -0800

    small changes to cutscene manager and camera controler

--- space-chase/scripts/cutscene_manager.gd
+++ space-chase/scripts/cutscene_manager.gd
@@ -5,8 +5,18 @@ extends Node
 Stores references to both players
 """
 
-# export player 1
-# export player 2
+# Consider storing references to ships, wall of death as well,
+# if we want more stuff in cutscenes.
+
+# Reference to PLAYER_1
+@export var player_1: Player
+# Reference to PLAYER_2
+@export var player_2: Player
+
 
 func start_cutscene() -> void:
+	pass
+
+
+func end_cutscene() -> void:
 	pass
