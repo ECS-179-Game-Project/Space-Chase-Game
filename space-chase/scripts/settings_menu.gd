794a0f8beba8068df65d6711a8fb1241b9ef41b8
BoiPlex <endplex@gmail.com>
2024-11-29 22:55:22 -0800

    Fixes to instant start debug option

--- space-chase/scripts/settings_menu.gd
+++ space-chase/scripts/settings_menu.gd
@@ -1,6 +1,7 @@
 class_name SettingsMenu
 extends Control
 
+var _main_menu:String = "res://scenes/main_menu.tscn"
 
 func _on_back_pressed() -> void:
-	SceneManager.change_scene("res://scenes/main_menu.tscn", {"skip_fade_out": true, "skip_fade_in": true})
+	SceneManager.change_scene(_main_menu, {"skip_fade_out": true, "skip_fade_in": true})
