f8fdd9c474b4de98a2041ecc906783be817a416e
BoiPlex <endplex@gmail.com>
2024-11-29 22:50:23 -0800

    Parallax scrolling and auto start debug option

--- space-chase/scripts/main_menu.gd
+++ space-chase/scripts/main_menu.gd
@@ -1,6 +1,14 @@
 class_name MainMenu
 extends Control
 
+@export_group("Debug Settings")
+@export var instant_start:bool = false
+
+
+func _on_ready() -> void:
+	if instant_start:
+		SceneManager.change_scene("res://scenes/world_1.tscn", {"skip_fade_out": true, "skip_fade_in": true})
+
 
 func _on_start_pressed() -> void:
 	SceneManager.change_scene("res://scenes/world_1.tscn", {"pattern": "circle"})
