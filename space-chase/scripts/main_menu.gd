dc4bf9a8fe4fc7c03d5cc29dd2309dd496388b7a
BoiPlex <endplex@gmail.com>
2024-12-01 15:02:28 -0800

    level_entered signal

--- space-chase/scripts/main_menu.gd
+++ space-chase/scripts/main_menu.gd
@@ -8,11 +8,18 @@ var _settings_menu:String = "res://scenes/settings_menu.tscn"
 
 func _on_ready() -> void:
 	if instant_start:
-		SceneManager.change_scene(_world_1, {"skip_fade_out": true, "skip_fade_in": true})
+		SceneManager.change_scene(_world_1, {
+			"skip_fade_out": true,
+			"skip_fade_in": true,
+			"on_fade_in": GameStateManager.level_entered.emit,
+		})
 
 
 func _on_start_pressed() -> void:
-	SceneManager.change_scene(_world_1, {"pattern": "circle"})
+	SceneManager.change_scene(_world_1, {
+		"pattern": "circle",
+		"on_fade_in": GameStateManager.level_entered.emit,
+		})
 
 
 func _on_settings_pressed() -> void:
