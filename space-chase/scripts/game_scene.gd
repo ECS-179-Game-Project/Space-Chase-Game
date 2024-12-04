e4a60a06a5a554aa77c2a58b3b6ea001d4b613e8
BoiPlex <endplex@gmail.com>
2024-12-04 13:36:26 -0800

    Intro cutscene and assets

---
+++ space-chase/scripts/game_scene.gd
@@ -0,0 +1,6 @@
+extends Node # Autoloaded
+
+# All scene strings should be here (non-menu scenes)
+ 
+var intro_cutscene: String = "res://scenes/intro_cutscene.tscn"
+var world_1: String = "res://scenes/world_1.tscn"
