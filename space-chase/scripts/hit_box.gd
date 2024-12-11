c0035f794363db3fb9f347854a5c09b00375ce12
patple <152749587+patple@users.noreply.github.com>
2024-12-10 20:07:17 -0800

    players will now get energy on opposing player death

--- space-chase/scripts/hit_box.gd
+++ space-chase/scripts/hit_box.gd
@@ -4,7 +4,6 @@ extends Area2D
 """
 Everything in the game is instakill
 """
-
 func _init() -> void:
 	set_collision_layer_value(4, true)
 	set_collision_mask_value(3, true) # Detect hurtboxes
@@ -12,7 +11,10 @@ func _init() -> void:
 
 
 func _on_hurtbox_entered(hurtbox: HurtBox) -> void:
+		
 	var target := hurtbox.owner
+	#current_energy = game_state_manager.get_player_energy(PlayerID.PLAYER_1)
+	#adjusted_energy = current_energy * 0.75
 	
 	if target.has_method("instakill"):
 		target.instakill()
