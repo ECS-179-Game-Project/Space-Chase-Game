30c64e9e1ce941c69c5828a1f3fb092f7d056358
RaghavsScarletSplendour <72269666+RaghavsScarletSplendour@users.noreply.github.com>
2024-12-11 14:24:14 -0800

    Cutscene audio added

--- space-chase/scripts/cutscene.gd
+++ space-chase/scripts/cutscene.gd
@@ -4,10 +4,16 @@ extends Node
 @export var skip_cutscene: bool = false
 
 @onready var cutscene_animation_player: AnimationPlayer = $CutsceneAnimationPlayer
+@onready var background_music: AudioStreamPlayer2D = $Audio/BackgroundMusic
+@onready var explosion_sound: AudioStreamPlayer2D = $Explosion/Explosion
+@onready var red_lazer_sound: AudioStreamPlayer2D = $RedShip/RedFire/LazerRedShip
+@onready var blue_lazer_sound: AudioStreamPlayer2D = $BlueShip/BlueFire/LazerBlueShip
+
 
 
 func _ready() -> void:
 	SceneManager.scene_loaded.connect(_on_cutscene_entered)
+	background_music.play()
 
 
 func _process(delta: float) -> void:
@@ -33,3 +39,11 @@ func _on_cutscene_entered() -> void:
 	else:
 		cutscene_animation_player.play("start_cutscene") # Will eventually call end_cutscene()
 		
+func play_explosion_sound():
+	explosion_sound.play()
+
+func play_red_ship_shoot_sound():
+	red_lazer_sound.play()
+
+func play_blue_ship_shoot_sound():
+	blue_lazer_sound.play()
