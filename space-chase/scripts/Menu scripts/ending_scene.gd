005960262fe8fdbb475c7d00ea60e8d72d5f7c9a
patple <152749587+patple@users.noreply.github.com>
2024-12-11 17:14:06 -0800

    tweaks to ending scene

--- space-chase/scripts/Menu scripts/ending_scene.gd
+++ space-chase/scripts/Menu scripts/ending_scene.gd
@@ -1,33 +1,29 @@
 extends Control
 
-
-@onready var game_state_manager = $/root/GameStateManager
 const PlayerID = GameStateManager.PlayerID
+
+@onready var game_state_manager: GameStateManager = $/root/GameStateManager
 @export var winning_score:float 
+
+func quit():
+	get_tree().paused = false
+	$".".visible = false
+	MenuManager.enter_menu()
+
 func pause():
-	#figure out a way to detect if the player is charging 
+	 #figure out a way to detect if the player is charging 
 	#should be another if statement? 
 	
-	if  GameStateManager.get_player_energy(PlayerID.PLAYER_1) >= winning_score:
+	if GameStateManager.get_player_energy(PlayerID.PLAYER_1) >= 1:
 	#ChargingStation.WIN_THRESHOLD:
 		#get_tree().paused = true
 		$".".visible = true
 		$VBoxContainer/Label.text = "PLAYER 1 WINS"
-	elif GameStateManager.get_player_energy(PlayerID.PLAYER_2) >= winning_score:
+	elif GameStateManager.get_player_energy(PlayerID.PLAYER_2) >= 1:
 	#ChargingStation.WIN_THRESHOLD:
 		#get_tree().paused = true
 		$".".visible = true
 		$VBoxContainer/Label.text = "PLAYER 2 WINS"
 
-func testEsc():
-	if get_tree().paused == false:
-		pause()
-		
-func _process(delta):
-	testEsc()
-
-
 func _on_quit_pressed() -> void:
-	get_tree().paused = false
-	$".".visible = false
-	MenuManager.enter_menu()
+	quit() # Replace with function body.
