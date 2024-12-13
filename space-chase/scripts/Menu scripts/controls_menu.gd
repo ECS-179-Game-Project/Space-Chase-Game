f9410e70c93cc35aebdf9e761168e55482a0c76f
BoiPlex <endplex@gmail.com>
2024-12-13 01:50:48 -0800

    Fixed controls toggles

--- space-chase/scripts/Menu scripts/controls_menu.gd
+++ space-chase/scripts/Menu scripts/controls_menu.gd
@@ -2,6 +2,12 @@ class_name ControlsMenu
 extends Control
 
 @onready var click_sound: AudioStreamPlayer2D = $Node2D/Click
+@onready var p1_jump_with_up: CheckButton = $Control/Settings/VBoxContainer/P1JumpWithUp
+@onready var p2_jump_with_up: CheckButton = $Control/Settings/VBoxContainer/P2JumpWithUp
+
+func _ready() -> void:
+	p1_jump_with_up.button_pressed = PlayerControls.use_up_as_jump[GameStateManager.PlayerID.PLAYER_1]
+	p2_jump_with_up.button_pressed = PlayerControls.use_up_as_jump[GameStateManager.PlayerID.PLAYER_2]
 
 
 func _on_back_pressed() -> void:
@@ -14,4 +20,4 @@ func _on_p1_jump_with_up_toggled(toggled_on: bool) -> void:
 
 
 func _on_p2_jump_with_up_toggled(toggled_on: bool) -> void:
-	PlayerControls.use_up_as_jump[GameStateManager.PlayerID.PLAYER_1] = toggled_on
+	PlayerControls.use_up_as_jump[GameStateManager.PlayerID.PLAYER_2] = toggled_on
